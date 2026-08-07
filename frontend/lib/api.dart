fae94fa66459aaf62c759f64545d91d08fdd2122
Pundiraryan <aryanpundir0606@gmail.com>
2026-08-07 22:34:06 +0530

    update backend url

--- frontend/lib/api.dart
+++ frontend/lib/api.dart
@@ -11,15 +11,7 @@ class GyaanApi {
       return envUrl;
     }
 
-    if (kIsWeb) {
-      return 'http://localhost:8000/api';
-    }
-
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8000/api';
-    }
-
-    return 'http://127.0.0.1:8000/api';
+    return 'https://gyaan-ws46.onrender.com/api';
   }
 
   Future<List<ArticleSummary>> fetchDailyInsights() async {
